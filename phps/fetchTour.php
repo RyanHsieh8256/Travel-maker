66b1833edba0b8754e557150906313b64c913dc4
joyce <joyce44528@gmail.com>
2022-03-17 18:36:24 +0800

    修改fetchTour的sql指令

--- phps/fetchTour.php
+++ phps/fetchTour.php
@@ -12,7 +12,7 @@
         
         case !empty($_GET['city']):
             $thing = $_GET['city'];
-            $sql = mysqli_query($conn, "select DISTINCT j3.journeyName,j3.journeyImg, j3.journeyNo from `city` c join spot s on c.cityNo = s.cityNo join journeyspot j2 on j2.spotNo = s.spotNo join journey j3 on j3.journeyNo = j2.journeyNo where c.cityNo = $thing");
+            $sql = mysqli_query($conn, "select DISTINCT j3.journeyName,j3.journeyImg, j3.journeyNo from `city` c join spot s on c.cityNo = s.cityNo join journeyspot j2 on j2.spotNo = s.spotNo join journey j3 on j3.journeyNo = j2.journeyNo where c.cityNo = $thing and j3.journeyState = '公開'");
             break;     
     }
 
