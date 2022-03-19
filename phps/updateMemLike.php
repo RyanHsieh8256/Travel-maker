cf124b4b224642c6d0266cf99a7d2ebddebfc0c6
joyce <joyce44528@gmail.com>
2022-03-20 00:18:48 +0800

    新增行程收藏增減到資料庫

---
+++ phps/updateMemLike.php
@@ -0,0 +1,35 @@
+<?php
+     $conn = mysqli_connect('cfd104g5.asuscomm.com','g3-1','cfd104_g3@hihi','g3');
+
+     
+
+    switch($_POST) {
+
+        case !empty($_POST['like']):
+            $datas = $_POST['like'];
+            $array = json_decode($datas,true);
+
+            foreach($array as $row) {
+                $sql = "INSERT IGNORE INTO journeycollect(memNo, journeyNo) VALUES ('".$row["memNo"]."','".$row["journeyNo"]."')";
+        
+                mysqli_query($conn,$sql);
+            }
+            break;
+
+        case !empty($_POST['unlike']):
+            $datas = $_POST['unlike'];
+            $array = json_decode($datas,true);
+
+
+            foreach($array as $row) {
+                $sql = "DELETE FROM journeycollect WHERE memNo = ".$row['memNo']." AND journeyNo = '".$row['journeyNo']."'";
+        
+                mysqli_query($conn,$sql);
+            }
+            
+            break;
+    }
+    
+
+
+?>
